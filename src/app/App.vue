31c92529d35e1fe63d1a14469b5da5331bce2f2b
Izzicoras <atomiep2@gmail.com>
2024-11-05 21:51:53 +0100

    Setup basic ui

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,12 +3,15 @@ import { useFileDialog } from '@vueuse/core';
 import { loadImageData } from './util/loadImageData';
 import { createImageSectionMarkings } from './util/createImageSectionMarkings';
 import { createImageFromMarkings } from './util/createImageFromMarkings';
+import { resolveCenterPointsFromMarkings } from './util/resolveCenterPointsFromMarkings';
+import { createGraphImage } from './util/createGraphImage';
 import { ref } from 'vue';
 import { resolveEdgesFromMarkings } from './util/resolveEdgesFromMarkings';
 import { uniq } from 'lodash-es';
 
 const url1 = ref('');
 const url2 = ref('');
+const url3 = ref('');
 
 const { files, open, reset, onCancel, onChange } = useFileDialog({
     accept: 'image/*', // Set to accept only image files
@@ -27,12 +30,16 @@ onChange(async (files) => {
             boundaries,
         } = createImageSectionMarkings(pixels, image.width, image.height);
 
-        console.log(resolveEdgesFromMarkings(markings, boundaries, image.width, image.height))
+        const centerPoints = resolveCenterPointsFromMarkings(markings, image.width, image.height);
+        const edges = resolveEdgesFromMarkings(markings, boundaries, image.width, image.height);
+
+        centerPoints.delete(-1);
+
+        console.log(centerPoints, edges);
 
         url1.value = (await createImageFromMarkings(markings, image.width, image.height)).src;
         url2.value = (await createImageFromMarkings(boundaries, image.width, image.height)).src;
-
-        console.log(uniq(markings))
+        url3.value = (await createGraphImage(centerPoints, edges, image.width, image.height)).src;
     }
 });
 </script>
@@ -42,5 +49,6 @@ onChange(async (files) => {
         <button type="button" @click="open">Choose file</button>
         <img :src="url1" alt="">
         <img :src="url2" alt="">
+        <img :src="url3" alt="">
     </div>
 </template>
