6f25845e4f34b3f5d9a4b5715ad468c3467a5f5a
Bennett Vollebregt <vollebregt.b@vepabins.nl>
2024-11-06 16:43:24 +0100

    Added watchEffect

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { useFileDialog } from '@vueuse/core';
+import { computedAsync, useFileDialog } from '@vueuse/core';
 import { loadImageData } from './util/loadImageData';
 import { createImageRegions } from './util/createImageRegions';
 import { createImageFromRegions } from './util/createImageFromRegions';
 import { resolveCenterPointsFromRegions } from './util/resolveCenterPointsFromRegions';
 import { createGraphImage } from './util/createGraphImage';
-import { ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { resolveEdgesFromRegions } from './util/resolveEdgesFromRegions';
 import { createRegionIndexMap } from './util/createRegionIndexMap';
 import { createFourColorSolver } from './util/createFourColorSolver';
@@ -16,39 +16,43 @@ const url2 = ref('');
 const url3 = ref('');
 const url4 = ref('');
 
-const { files, open, reset, onCancel, onChange } = useFileDialog({
+const { files, open } = useFileDialog({
     accept: 'image/*', // Set to accept only image files
     multiple: false,
 });
 
-onChange(async (files) => {
-    if (files && files[0]) {
-        const {
-            image,
-            pixels,
-        } = await loadImageData(files[0]);
+const imageAndPixels = computedAsync(async () => files.value && await loadImageData(files.value[0]));
+const image = computed(() => imageAndPixels.value?.image);
+const pixels = computed(() => imageAndPixels.value?.pixels);
 
-        const {
-            regions,
-            boundaries,
-        } = createImageRegions(pixels, image.width, image.height);
-
-        const regionMap = createRegionIndexMap(regions);
-
-        const centerPoints = resolveCenterPointsFromRegions(regions, regionMap, image.width);
-        const edges = resolveEdgesFromRegions(regions, boundaries, image.width, image.height);
-        const colorMap = createFourColorSolver({
-            edges,
-        });
-
-        centerPoints.delete(-1);
-        centerPoints.delete(0);
-
-        url1.value = (await createImageFromRegions(regions, image.width, image.height)).src;
-        url2.value = (await createImageFromRegions(boundaries, image.width, image.height)).src;
-        url3.value = (await createGraphImage(centerPoints, edges, image.width, image.height)).src;
-        url4.value = (await createColoredRegionImage(pixels, regionMap, colorMap, image.width, image.height)).src;
+watchEffect(async () => {
+    if ( ! (image.value && pixels.value)) {
+        return;
     }
+
+    const width = image.value.width;
+    const height = image.value.height;
+
+    const {
+        regions,
+        boundaries,
+    } = createImageRegions(pixels.value, width, height);
+
+    const regionMap = createRegionIndexMap(regions);
+
+    const centerPoints = resolveCenterPointsFromRegions(regions, regionMap, width);
+    const edges = resolveEdgesFromRegions(regions, boundaries, width, height);
+    const colorMap = createFourColorSolver({
+        edges,
+    });
+
+    centerPoints.delete(-1);
+    centerPoints.delete(0);
+
+    url1.value = (await createImageFromRegions(regions, width, height)).src;
+    url2.value = (await createImageFromRegions(boundaries, width, height)).src;
+    url3.value = (await createGraphImage(centerPoints, edges, width, height)).src;
+    url4.value = (await createColoredRegionImage(pixels.value, regionMap, colorMap, width, height)).src;
 });
 </script>
 
